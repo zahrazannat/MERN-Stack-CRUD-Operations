8e76ed5ba61958f7ef8150651ba6604d7223f65c
zahrazannat <zahra.zannat@g.bracu.ac.bd>
2023-11-09 00:50:30 +0600

    Add files via upload

---
+++ CRUD/server/index.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const UserModel = require('./models/Users');
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+mongoose.connect("mongodb://127.0.0.1:27017/CRUD" ,);
+
+app.get('/', (req,res) => {
+    UserModel.find({})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.get('/getUser/:id', (req, res) =>{
+    const id = req.params.id;
+    UserModel.findById({_id:id})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.put('/updateUser/:id', (req, res) => {
+    const id = req.params.id;
+    UserModel.findByIdAndUpdate({_id: id}, {
+        name: req.body.name, 
+        email: req.body.email, 
+        age: req.body.age})
+    .then(users => res.json(users))
+    .catch(err => res.json(err))
+})
+
+app.delete('/deleteUser/:id', (req,res) => {
+    const id =req.params.id;
+    UserModel.findByIdAndDelete({_id: id})
+    .then(res => res.json(res))
+    .catch(err => res.json(err))
+})
+
+app.post("/createUser", async (req, res) => {
+    try {
+        const user = await UserModel.create(req.body);
+    res.json(user);
+  } catch (err) {
+    res.status(500).json({ error: 'Could not create the user', details: err.message });
+  }
+});
+
+app.listen(3001, () => {
+    console.log("Server is Running on port 3001");
+});
